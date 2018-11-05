ccaa1a90ca9ce65a15561421fe3a3ca63ca17436
vianneyJouhet <vianney.jouhet@gmail.com>
2018-11-05 18:19:31 +0100

    Add SAN import

--- shrine/ressources/import-cert.sh
+++ shrine/ressources/import-cert.sh
@@ -3,5 +3,5 @@ source $SECRETS
 
 keytool -delete -v -alias shrine-hub-ca -keystore shrine.keystore -storepass $KEYSTORE_PASSWORD
 keytool  -import -v -alias shrine-hub-ca -file /opt/cert/shrine-hub-ca.crt -keystore shrine.keystore -storepass $KEYSTORE_PASSWORD
-keytool -import -v -alias $KEYSTORE_ALIAS -file /opt/cert/${KEYSTORE_ALIAS}-signed.crt -keystore shrine.keystore -storepass $KEYSTORE_PASSWORD -keypass $KEYSTORE_PASSWORD -trustcacerts
+keytool -import -v -alias $KEYSTORE_ALIAS -file /opt/cert/${KEYSTORE_ALIAS}-signed.crt -keystore shrine.keystore -storepass $KEYSTORE_PASSWORD -keypass $KEYSTORE_PASSWORD -trustcacerts -ext SAN=dns:$KEYSTORE_ALIAS,dns:shrine
 keytool -import -list -v -keystore shrine.keystore -storepass $KEYSTORE_PASSWORD
