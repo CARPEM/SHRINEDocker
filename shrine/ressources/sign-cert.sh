006fa7656ffd8f2fbc0738a9df69df0a1354c3a8
vianneyJouhet <vianney.jouhet@gmail.com>
2018-11-06 17:12:37 +0100

    fix requests

--- shrine/ressources/sign-cert.sh
+++ shrine/ressources/sign-cert.sh
@@ -9,4 +9,4 @@ cd /opt/shrine
 /usr/lib/ssl/misc/CA.pl -sign
 mv /opt/shrine/newcert.pem /opt/cert/${VAR}-signed.crt
 keytool  -import -v -alias ${VAR} -file /opt/cert/${VAR}_HTTPS.cer -keystore shrine.keystore -storepass $KEYSTORE_PASSWORD
-sed -i "s#downstreamNodes{#downstreamNodes{\n           \"$2\" = \"https://${VAR}:6443/shrine/rest/adapter/resquests\"#g" tomcat/lib/shrine.conf
+sed -i "s#downstreamNodes{#downstreamNodes{\n           \"$2\" = \"https://${VAR}:6443/shrine/rest/adapter/requests\"#g" tomcat/lib/shrine.conf
